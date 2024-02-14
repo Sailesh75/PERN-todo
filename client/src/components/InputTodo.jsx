ccb561fd24e59a68a0848254d3564c650703e638
Sailesh Kafle <saileshkafle47@gmail.com>
2024-02-14 10:14:03 +0545

    delete all button added

--- client/src/components/InputTodo.jsx
+++ client/src/components/InputTodo.jsx
@@ -4,13 +4,28 @@ import axios from "axios";
 const InputTodo = () => {
   const [description, setDescription] = useState("");
 
-  const onSubmitForm = async (e) => {
+  const addTodo = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`http://localhost:5000/todos`, {
-        description,
-      });
-      // once request has been sent it refreshes the window showing the change
+      if (description === "") {
+        window.alert("Write SM please!!");
+      } else {
+        const response = await axios.post(`http://localhost:5000/todos`, {
+          description,
+        });
+        // once request has been sent it refreshes the window showing the change
+        window.location = "/";
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteAllTodo = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.delete(`http://localhost:5000/todos`);
+      console.log(response);
       window.location = "/";
     } catch (error) {
       console.log(error);
@@ -20,14 +35,19 @@ const InputTodo = () => {
   return (
     <>
       <h1 className="m-4 text-center">Welcome to PERN todo app</h1>
-      <form className="d-flex p-2" onSubmit={onSubmitForm}>
+      <form className="d-flex p-2">
         <input
           type="text"
-          className="form-control"
+          className="form-control m-5"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         ></input>
-        <button className="btn btn-primary">Add</button>
+        <button className="btn btn-primary m-5" onClick={addTodo}>
+          Add
+        </button>
+        <button className="btn btn-danger m-5" onClick={deleteAllTodo}>
+          Delete all
+        </button>
       </form>
     </>
   );
