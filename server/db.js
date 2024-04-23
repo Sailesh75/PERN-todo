c9477247409fcc559139478e62ea4d1b64b8be31
Sailesh Kafle <saileshkafle47@gmail.com>
2024-04-23 15:57:42 +0545

    env file added

--- server/db.js
+++ server/db.js
@@ -1,12 +1,19 @@
 // connect database and server in express
-const { Pool } = require("pg");   //destructuring pg model to extract only the Pool object from it
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_NAME = process.env.DB_NAME;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "myadminql",
-  host: "localhost",
-  port: 5432,
-  database: "perntodo",
+  user: DB_USER,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  port: DB_PORT,
+  database: DB_NAME,
 });
 
 module.exports = pool;
