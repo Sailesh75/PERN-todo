759cd74f1feb637b4573e53b76f4ba7464bf602f
Sailesh Kafle <saileshkafle47@gmail.com>
2024-05-14 22:35:34 +0545

    proxy server added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "https://todoapp-bnx4.onrender.com",
+      "/auth": "https://todoapp-bnx4.onrender.com",
+      "/dashboard": "https://todoapp-bnx4.onrender.com",
+    },
+  },
   plugins: [react()],
-})
+});
